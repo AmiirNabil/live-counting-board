a4ed9326e2538f01ef614ff53ef704fab9d612e6
AmiirNabil <Amir.N.George@gmail.com>
2024-11-15 15:42:35 +0200

    removed supplying validator class in the constructor

--- src/test/java/com/assessment/countingBoard/CountingBoardTest.java
+++ src/test/java/com/assessment/countingBoard/CountingBoardTest.java
@@ -11,7 +11,7 @@ public class CountingBoardTest {
 
     @BeforeEach
     void setUp() {
-        countingBoard = new CountingBoardDefault(new MatchValidatorImpl());
+        countingBoard = new CountingBoardDefault();
     }
 
     @Test
